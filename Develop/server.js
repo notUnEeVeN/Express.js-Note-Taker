d5aedbdfb9886a2c37250ea32e68ac41443de6b9
notUnEeVeN <tybalt.mallet@gmail.com>
2023-08-30 13:53:32 -0700

    fixed express static folder path in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 
 
 app.get('/notes', (req, res) => {
--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: node Develop/server.js
-web: node Develop/public/assets/js/index.js
